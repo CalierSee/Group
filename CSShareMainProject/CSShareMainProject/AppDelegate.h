2289b54e8d956d0f725149370c5bf04f4f32300f
景彦铭 <436005247@qq.com>
2017-07-31 18:22:00 +0800

    init

---
+++ CSShareMainProject/CSShareMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CSShareMainProject
+//
+//  Created by 景彦铭 on 2017/7/31.
+//  Copyright © 2017年 景彦铭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
